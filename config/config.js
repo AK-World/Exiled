a13534b7d47c60ac4ead3f86c5054977a9e8db75
Akash Kumar <33202040+AK-World@users.noreply.github.com>
2017-11-13 10:59:49 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -29,7 +29,7 @@ exports.bindaddress = '0.0.0.0';
 exports.workers = 1;
 
 //serverIp for parsing of custom avatars, put your server's ip here
-exports.serverIp = 'impulsevps.cleverapps.io';
+exports.serverIp = 'princesky.cleverapps.io';
 
 // TODO: allow SSL to actually be possible to use for third-party servers at
 // some point.
