012d8e02e803bb43f1b6276a88c39622a5140bdc
Akash Kumar <33202040+AK-World@users.noreply.github.com>
2017-11-14 07:59:08 +0530

    Update betting-game.js

--- server-plugins/betting-game.js
+++ server-plugins/betting-game.js
@@ -28,6 +28,7 @@ function isOdd(n) {
 
 
 exports.commands = {
+	shellgame: {
 	bet: function (target, room, user) {
 		let firstDice = diceOne();
 		let secondDice = diceTwo();
@@ -39,7 +40,7 @@ exports.commands = {
 		let amount = Db('money').get(user.userid, 0);
 		if (room.id !== 'casino' && !~developers.indexOf(this.userid)) return this.errorReply('betting games can only be used in Casino');
 
-		if (amount < 2) return this.errorReply("You don't have enough bucks for the bet.");
+		if (amount < 2) return this.errorReply("You don't have enough snow balls for the bet.");
 
 		if (!target) return this.parse('/help bet');
 
@@ -48,7 +49,7 @@ exports.commands = {
 			Db('money').set(user.userid, amount - 2).get(user.userid);
 			if (isOdd(totalDice) && house < 87) {
 				this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><font style="color: #666; font-style: italic;">' + user.name + ' betted on "' + choice + '".</font><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font>\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">' + totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You Win!!</h2></center></div></div></div>');
-				Db('money').set(user.userid, amount + 2).get(user.userid);
+				Db('money').set(user.userid, amount + 3).get(user.userid);
 			} else {
 				this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><font style="color: #666; font-style: italic;">' + user.name + ' betted on "' + choice + '".</font><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font><center><h2 style="color: #444">You lose... better luck next time!</h2></center></div></div></div>');
 			}
@@ -57,7 +58,7 @@ exports.commands = {
 			Db('money').set(user.userid, amount - 2).get(user.userid);
 			if (isEven(totalDice) && house < 87) {
 				this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><font style="color: #666; font-style: italic;">' + user.name + ' betted on "' + choice + '".</font><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font>\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">' + totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You Win!!</h2></center></div></div></div>');
-				Db('money').set(user.userid, amount + 2).get(user.userid);
+				Db('money').set(user.userid, amount + 3).get(user.userid);
 			} else {
 				this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><font style="color: #666; font-style: italic;">' + user.name + ' betted on "' + choice + '".</font><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font><center><h2 style="color: #444">You lose... better luck next time!</h2></center></div></div></div>');
 			}
@@ -66,7 +67,7 @@ exports.commands = {
 			Db('money').set(user.userid, amount - 2).get(user.userid);
 			if (totalDice === 7 && house < 70) {
 				this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><font style="color: #666; font-style: italic;">' + user.name + ' betted on "' + choice + '".</font><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font>\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">' + totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You Win!!</h2></center></div></div></div>');
-				Db('money').set(user.userid, amount + 14).get(user.userid);
+				Db('money').set(user.userid, amount + 5).get(user.userid);
 			} else {
 				this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><font style="color: #666; font-style: italic;">' + user.name + ' betted on "' + choice + '".</font><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font><center><h2 style="color: #444">You lose... better luck next time!</h2></center></div></div></div>');
 			}
@@ -74,6 +75,6 @@ exports.commands = {
 		default:
 			this.errorReply("Not a valid bet.");
 		}
-	},
-	bethelp: ["/bet [type] - rolls two dices and adds the two to make a final number. Choose between odd, even or seven. If you guess correctly you win bucks (betting for seven and winning awards more bucks)."],
+	},},
+	shellgamehelp: ["/shellgame bet [type] - rolls two dices and adds the two to make a final number. Choose between odd, even or seven. If you guess correctly you win snow balls (betting for seven and winning awards more snow ballls)."],
 };
